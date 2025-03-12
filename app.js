78e14b7862bf448a3f23cdb521f19f676acc29a1
akshita03par <akshitaparmar2023@gmail.com>
2025-03-12 21:11:36 +0530

    navbar and herosection completed

---
+++ app.js
@@ -0,0 +1,7 @@
+ // Mobile menu toggle
+ 
+ const menuBtn = document.getElementById("mobile-menu-button");
+ const mobileMenu = document.getElementById("mobile-menu");
+ menuBtn.addEventListener("click", () => {
+   mobileMenu.classList.toggle("hidden");
+ });
