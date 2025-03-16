2ccd15d268bf5cb40b52cd7e96772d04d7d18aaa
akshita03par <akshitaparmar2023@gmail.com>
2025-03-16 21:08:11 +0530

    project section complete

--- app.js
+++ app.js
@@ -5,3 +5,35 @@
  menuBtn.addEventListener("click", () => {
    mobileMenu.classList.toggle("hidden");
  });
+
+// Typed text animation
+
+var typeData = new Typed(".role", {
+  strings: [
+    "Full Stack Developer",
+    "Web Developer",
+    "UI-UX Designer",
+    "Backend Developer",
+    "Coder",
+  ],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 1000,
+});
+
+ // swiper
+
+  const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
