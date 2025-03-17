a476ae0e56c219aeeb9d1c0aa29dc7a512a96266
akshita03par <akshitaparmar2023@gmail.com>
2025-03-17 20:55:34 +0530

    changes in js

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
- // Mobile menu toggle
- 
- const menuBtn = document.getElementById("mobile-menu-button");
- const mobileMenu = document.getElementById("mobile-menu");
- menuBtn.addEventListener("click", () => {
-   mobileMenu.classList.toggle("hidden");
- });
+// Mobile menu toggle
+
+const menuBtn = document.getElementById("mobile-menu-button");
+const mobileMenu = document.getElementById("mobile-menu");
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
 
 // Typed text animation
 
 var typeData = new Typed(".role", {
   strings: [
-    "Full Stack Developer",
+    // "Full Stack Developer",
     "Web Developer",
-    "UI-UX Designer",
+    // "UI-UX Designer",
     "Backend Developer",
     "Coder",
   ],
@@ -22,18 +22,18 @@ var typeData = new Typed(".role", {
   backDelay: 1000,
 });
 
- // swiper
+// swiper
 
-  const swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
